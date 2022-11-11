1878642a774c646599f27f97898af2ade932196d
JavaVega <jcarrascalgarcia@gmail.com>
2022-11-11 04:36:03 -0500

    aprendiendo diccionario en python

---
+++ diccionario.py
@@ -0,0 +1,12 @@
+def run():
+
+    poblacion_latan = {
+        "Colombia" : 123456,
+        "Venezuela" : 654321,
+        "Brazil" : 135790,
+    }
+    for pais in poblacion_latan.items():
+        print(pais)
+
+if __name__ == "__main__":
+    run()
